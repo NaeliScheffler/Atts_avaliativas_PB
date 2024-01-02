c7419f7fe590a576ad6c44a0673fb0947a8942ce
Bruno <brunobuenopovoa01@gmail.com>
2024-01-02 16:26:08 -0300

    Adicionando exercício 7

---
+++ Atividade_7/Ex7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ex7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Monte uma lista de numeros e lhe direi a media deles: ");
+        System.out.println("Tamanho da lista: ");
+        int tam = in.nextInt();
+        Double [] vetorNumeros = new Double[tam];
+        Double media = 0.0;
+
+        for (int i = 0; i < tam; i++) {
+            System.out.print(i+1 + "° Valor: ");
+            vetorNumeros[i] = in.nextDouble();
+            media += vetorNumeros[i];
+        }
+
+        media = media / tam;
+
+        System.out.println("Media dos valores = " + media);
+
+    }
+}
